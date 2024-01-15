2359b7636582acde8051eee5341d09896cb38d68
Jordan Primas <jorprimas20@gmail.com>
2024-01-15 15:51:34 -0800

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        sorted_list = []
+        for anagram in possible_anagrams:
+            if sorted(anagram) == sorted(self.word):
+                sorted_list.append(anagram)
+        return sorted_list
+
+
+
+
+
+
